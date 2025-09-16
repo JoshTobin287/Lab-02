02063f811325c002256d05bf2de7a84253b10034
Josh Tobin <C00309712@setu.ie>
2025-09-16 11:52:10 +0000

    updates

--- lab2.py
+++ lab2.py
@@ -1,9 +1,10 @@
+import re
+
 with open('auth.log', 'r') as f:
     for line in f:
-        line = auth.log.readlines()
+        pattern = r"\d+\.\d+\.\d+\.\d+"
         print(line.strip())
+        ips = []
+        print(re.findall(pattern, line))
 
-import re
-pattern = r"\d+\.\d+\.\d+\.\d+"
 
-print(re.findall(pattern, line))
